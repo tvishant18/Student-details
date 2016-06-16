86b7db1144994ff9d1c0d84c4a1ff3829b324f2b
vishant thakur <vishant thakur@vishant>
2016-06-16 17:17:24 +0530

    practice assignment

---
+++ StudentDetails/src/A.java
@@ -0,0 +1,21 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student obj1= new Student();
+obj1.setId(1);
+obj1.setAge(19);
+obj1.setName("Vishant");
+System.out.println(obj1.getName() +" is " + obj1.getAge()+" years old");
+Student obj2=new Student();
+obj2.setId(2);
+obj2.setAge(19);
+obj2.setName("yashasvi");
+System.out.println(obj2.getName() + " is " + obj2.getAge() + " years old");
+
+
+
+	}
+
+}
---
+++ StudentDetails/src/Student.java
@@ -0,0 +1,24 @@
+
+public class Student {
+int id;
+int age;
+String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
